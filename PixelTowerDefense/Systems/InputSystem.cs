515b11ab80984e92f56376a8f7e25c0b378a3bf3
Byron <48884858+bkgryder@users.noreply.github.com>
2025-07-16 08:07:03 -0500

    Add Random.NextFloat extension and move to helpers

--- PixelTowerDefense/Systems/InputSystem.cs
+++ PixelTowerDefense/Systems/InputSystem.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using PixelTowerDefense.Entities;
-using PixelTowerDefense.Utils;
+using PixelTowerDefense.Helpers;
 using PixelTowerDefense;
 
 namespace PixelTowerDefense.Systems
