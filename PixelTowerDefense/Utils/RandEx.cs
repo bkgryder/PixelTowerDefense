fc2384074005c87c187ce300b38108fdc7b5a0a0
Byron <48884858+bkgryder@users.noreply.github.com>
2025-07-19 16:10:57 -0500

    Use instance random in NextFloat

--- PixelTowerDefense/Utils/RandEx.cs
+++ PixelTowerDefense/Utils/RandEx.cs
@@ -4,8 +4,7 @@ namespace PixelTowerDefense.Utils
 {
     public static class RandEx
     {
-        private static Random _r = new Random();
         public static float NextFloat(this Random _, float min, float max)
-            => min + (float)_r.NextDouble() * (max - min);
+            => min + (float)_.NextDouble() * (max - min);
     }
 }
