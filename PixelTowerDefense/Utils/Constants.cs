900cb9f4a5a5a0ac61792e0c7f1d6478af234491
Byron <48884858+bkgryder@users.noreply.github.com>
2025-07-20 18:37:03 -0500

    Regrow berries only under rain cloud

--- PixelTowerDefense/Utils/Constants.cs
+++ PixelTowerDefense/Utils/Constants.cs
@@ -90,6 +90,7 @@ using Microsoft.Xna.Framework;
         public const float HUNGER_THRESHOLD = 5f;
         public const float HARVEST_RANGE = 2f;
         public const int BUSH_BERRIES = 5;
+        public const float BUSH_REGROW_INTERVAL = 1f;
 
         // Precipitate ability
         public const float PRECIPITATE_DROP_SPEED = 30f;
