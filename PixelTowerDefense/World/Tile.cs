41f294d9c450cb9a4ce1b0b65db614b5f6a020a9
Byron <48884858+bkgryder@users.noreply.github.com>
2025-07-26 05:42:15 -0500

    Add basic chunked grid world

---
+++ PixelTowerDefense/World/Tile.cs
@@ -0,0 +1,14 @@
+namespace PixelTowerDefense.World
+{
+    public enum TileType
+    {
+        Empty,
+        Grass,
+        Dirt
+    }
+
+    public struct Tile
+    {
+        public TileType Type;
+    }
+}
